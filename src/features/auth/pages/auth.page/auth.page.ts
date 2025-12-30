3f42a70633391be51972b8497f9f1183e1ccb88e
Karpov <yurick@bk.ru>
2025-12-30 17:31:47 +0300

    feat: Страница входа

--- src/features/auth/pages/auth.page/auth.page.ts
+++ src/features/auth/pages/auth.page/auth.page.ts
@@ -1,19 +1,50 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatCard } from '@angular/material/card';
+import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatCheckbox } from '@angular/material/checkbox';
 import { AuthService } from '@core/services';
 import { Router } from '@angular/router';
-import { MatButton } from '@angular/material/button';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
 	selector: 'app-auth.page',
-	imports: [MatButton],
+	imports: [
+		MatButton,
+		MatCard,
+		MatFormField,
+		MatLabel,
+		FormsModule,
+		MatInput,
+		MatCheckbox,
+		TranslatePipe,
+	],
 	templateUrl: './auth.page.html',
+	styleUrl: './auth.page.scss',
 })
 export class AuthPage {
 	private auth = inject(AuthService);
 	private router = inject(Router);
 
-	async login(): Promise<void> {
-		this.auth.login();
-		await this.router.navigate(['/catalog']);
+	login = signal('');
+	password = signal('');
+	remember = signal(false);
+
+	loginValid = computed(() => this.login().length > 0);
+	passwordValid = computed(() => this.password().length > 0);
+	formValid = computed(() => this.loginValid() && this.passwordValid());
+
+	async onLogin() {
+		if (this.formValid()) {
+			console.log('Форма отправлена:', {
+				login: this.login(),
+				password: this.password(),
+				remember: this.remember(),
+			});
+
+			this.auth.login();
+			await this.router.navigate(['/catalog']);
+		}
 	}
 }
